45e3cca6ce56f8c156abf0a91b60410f216d4f6f
Maria-Varghese <maria.varghese@mths.ca>
2025-01-14 10:23:45 -0500

    Servo code for final project

--- main.ts
+++ main.ts
@@ -1,8 +1,27 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Maria Varghese
+ * Created on: Jan 2025
+ * This program uses servo and robotbit 
 */
 
-basic.showString('Hello, World!')
+//variable
+const S1 = robotbit.Servos.S1
+
+//setup
+basic.showIcon(IconNames.Happy)
+
+//when button A is pressed 
+input.onButtonPressed(Button.A, function () {
+    robotbit.Servo(S1, 156)
+    basic.showIcon(IconNames.Happy)
+})
+
+//when button B is pressed 
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(S1, 90)
+    basic.showIcon(IconNames.Happy)
+})
+
+//cleanup
+basic.clearScreen()
